6b86868f0cf76625234d36d7a316d432d7570376
Melisa Budini <melixbq@gmail.com>
2020-09-18 16:22:04 -0300

    ADD: image, page three

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/two'
+  get 'pages/three'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#one'
 end
